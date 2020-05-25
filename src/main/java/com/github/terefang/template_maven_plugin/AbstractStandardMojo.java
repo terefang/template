7114d321c411f1ee5968205f7d101e3c79e02556
BuildTools <unconfigured@null.spigotmc.org>
2020-05-25 19:50:47 +0200

    2020.5.8

--- src/main/java/com/github/terefang/template_maven_plugin/AbstractStandardMojo.java
+++ src/main/java/com/github/terefang/template_maven_plugin/AbstractStandardMojo.java
@@ -39,7 +39,7 @@ public abstract class AbstractStandardMojo extends AbstractTmpMojo {
     /**
      * local context extensions
      */
-    @Parameter(defaultValue = ".yaml .yml .json .hson .hjson .plist .properties")
+    @Parameter(defaultValue = ".yaml .yml .json .hson .hjson .plist .ini .properties")
     protected String localContextExtensions;
 
     /**
