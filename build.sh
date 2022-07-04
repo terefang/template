eef9a6349b3bdb3665be0c06d5e367edc2a2cbad
BuildTools <unconfigured@null.spigotmc.org>
2022-07-04 11:42:08 +0200

    2022.7.375

--- build.sh
+++ build.sh
@@ -8,13 +8,37 @@ OPTS="-DskipTests=true"
 
 while test ! -z "$1" ; do
   case "$1" in
+    -setversion*)
+      VALUE="${2}"
+      (cd $bDIR && mvn build-helper:parse-version versions:set \
+                -DnewVersion="${VALUE}" ) || exit 1
+      shift
+      ;;
+    -setmajor*)
+      VALUE="${2}"
+      (cd $bDIR && mvn build-helper:parse-version versions:set \
+                -DnewVersion="${VALUE}.\${parsedVersion.minorVersion}.\${parsedVersion.nextIncrementalVersion}" ) || exit 1
+      shift
+      ;;
     -major*)
       (cd $bDIR && mvn build-helper:parse-version versions:set \
                 -DnewVersion="\${parsedVersion.nextMajorVersion}.1.\${parsedVersion.nextIncrementalVersion}" ) || exit 1
       ;;
+    -setminor*)
+      VALUE="${2}"
+      (cd $bDIR && mvn build-helper:parse-version versions:set \
+                -DnewVersion="\${parsedVersion.majorVersion}.${VALUE}.\${parsedVersion.nextIncrementalVersion}" ) || exit 1
+      shift
+      ;;
     -minor*)
       (cd $bDIR && mvn build-helper:parse-version versions:set \
                 -DnewVersion="\${parsedVersion.majorVersion}.\${parsedVersion.nextMinorVersion}.\${parsedVersion.nextIncrementalVersion}" ) || exit 1
+      ;;
+    -setrelease*)
+      VALUE="${2}"
+      (cd $bDIR && mvn -X build-helper:parse-version versions:set \
+                -DnewVersion="\${parsedVersion.majorVersion}.\${parsedVersion.minorVersion}.${VALUE}" ) || exit 1
+      shift
       ;;
     -release*)
       (cd $bDIR && mvn -X build-helper:parse-version versions:set \
